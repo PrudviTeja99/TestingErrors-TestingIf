faae7b601733e92246ed6f7016be98cc22195261
769901 <prudvitejabandam987@gmail.com>
2022-11-03 10:58:47 +0530

    first errors testing

---
+++ src/main/java/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        int n=2;
+        if(n=1){
+            System.out.println("Hello");
+        }
+        else{
+            System.out.println("Hi");
+        }
+    }
+}
